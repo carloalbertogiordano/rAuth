704d06db6a2ff3c77f56ba0b74501512e0c6fb38
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2023-07-24 10:16:55 +0200

    Added support for configuration

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,23 @@ use rocket_auth::{Users};
 mod api;
 use api::api_handler::*;
 
+use rocket::serde::Deserialize;
 
+#[derive(Deserialize)]
+struct AppConfig {
+    database_path: String,
+    rocket_mountpoint: String,
+}
 
 #[rocket::main]
 async fn main() {
-    let users = Users::open_rusqlite("/tmp/mydb.db").expect("Failed to open database");
+    let figment = rocket::Config::figment();
+    let config: AppConfig = figment.extract().expect("Failed to extract config");
+
+    let users = Users::open_rusqlite(&config.database_path).expect("Failed to open database");
 
     let result = rocket::build()
-        .mount("/", routes![signup, login, logout, status])
+        .mount(&config.rocket_mountpoint, routes![signup, login, logout, status])
         .manage(users)
         .launch()
         .await;
